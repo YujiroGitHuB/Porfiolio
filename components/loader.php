68025daa699c7483e011a6ed61bdc38a9d84572f
Charles Nixon Cayading <charlesnixoncayading@gmail.com>
2025-11-30 10:53:45 +0800

    now

---
+++ components/loader.php
@@ -0,0 +1,12 @@
+ <!-- Loader -->
+ <div id="loader">
+     <!-- From Uiverse.io by Pradeepsaranbishnoi -->
+     <div class="circ">
+         <div class="load">Loading . . . </div>
+         <div class="hands"></div>
+         <div class="body"></div>
+         <div class="head">
+             <div class="eye"></div>
+         </div>
+     </div>
+ </div>
---
+++ components/navbar.php
@@ -0,0 +1,32 @@
+<!-- Navbar -->
+<nav id="navbar" class="navbar navbar-dark bg-transparent fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand fw-bold" href="index.html">CNCC
+            <img id="navbar-logo" src="images/secure_17122475.gif" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar"
+            aria-labelledby="offcanvasDarkNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title">Menu</h5>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav text-center">
+                    <li class="nav-item"><a class="nav-link" href="index.html"><i class="fa-solid fa-house"></i>
+                            Home</a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="#aboutme"><i class="fa-solid fa-user"></i> About
+                            Me</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#projects"><i class="fa-solid fa-code"></i>
+                            Projects</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#skills"><i class="fa-solid fa-code"></i>
+                            Skills</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
